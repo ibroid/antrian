da88322d47d800beed60663763ffbb99f015a159
ibroid <maulana858@gmail.com>
2024-05-13 21:56:22 +0700

    save from prod

--- application/views/pelayanan/pelayanan_produk.php
+++ application/views/pelayanan/pelayanan_produk.php
@@ -5,8 +5,8 @@
     <div class="col-12">
       <div class="card">
         <div class="card-body">
-          <!-- <h4 class="card-title">Pesanan Produk Pengadilan</h4>
-          <table class="table table-hovered table">
+          <h4 class="card-title">Pesanan Produk Pengadilan</h4>
+          <table class="table table-hovered table" id="table-produk">
             <thead>
               <tr>
                 <th>Nomor Antrian</th>
@@ -17,13 +17,27 @@
                 <th>Aksi</th>
               </tr>
             </thead>
-          </table> -->
-          <div class="text-center">
-            <h1>Halam Produk sedang dalam pembahasan lokasi</h1>
-          </div>
+            <tbody>
+              <?php foreach ($antrian_produk as $d) : ?>
+                <tr>
+                  <td><?= $d->nomor_antrian ?></td>
+                  <td><?= $d->produk->nomor_perkara ?></td>
+                  <td><?= $d->produk->nama_pengambil ?></td>
+                  <td><?= $d->produk->jenis_produk ?></td>
+                  <td></td>
+                  <td><button class="btn btn-warning">Proses</button></td>
+                </tr>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
         </div>
       </div>
-
     </div>
   </div>
 </div>
+
+<script>
+  window.addEventListener("load", function() {
+    $("#table-produk").DataTable()
+  })
+</script>
